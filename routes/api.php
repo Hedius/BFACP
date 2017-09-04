a51f4b0dfe9b0ff8c4f9c16b6cc2ba16f8f7d394
Prophet731 <webmaster@amurray.me>
2017-09-04 03:00:18 -0700

    Updated routes/api

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,42 @@
 <?php
 
-use Illuminate\Http\Request;
+use BFACP\Http\Resources\Game as GameResource;
+use BFACP\Http\Resources\Player as PlayerResource;
+use BFACP\Http\Resources\Server as ServerResource;
+use BFACP\Http\Resources\Adkats\Record as RecordResource;
+use BFACP\Realm\Adkats\Record;
+use BFACP\Realm\Game;
+use BFACP\Realm\Player;
+use BFACP\Realm\Server;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('player/{id?}', function ($id = null) {
+    if (! empty($id)) {
+        return new PlayerResource(Player::findOrFail($id));
+    }
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    return PlayerResource::collection(Player::paginate(100));
+})->where('id', '[0-9]+');
+
+Route::get('game/{name?}', function ($name = null) {
+    if (! empty($name)) {
+        return new GameResource(Game::where('Name', $name)->firstOrFail());
+    }
+
+    return GameResource::collection(Game::all());
+})->where('name', '[A-Za-z]+');
+
+Route::get('server/{id?}', function ($id = null) {
+    if (! empty($id)) {
+        return new ServerResource(Server::findOrFail($id));
+    }
+
+    return ServerResource::collection(Server::paginate(30));
+})->where('id', '[0-9]+');
+
+Route::get('record/{id?}', function ($id = null) {
+    if (! empty($id)) {
+        return new RecordResource(Record::findOrFail($id));
+    }
+
+    return RecordResource::collection(Record::paginate(30));
+})->where('id', '[0-9]+');
