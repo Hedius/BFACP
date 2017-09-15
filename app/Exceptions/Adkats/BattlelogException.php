c17a19a3d090c11808081f69745f584c34dbbe40
Prophet731 <webmaster@amurray.me>
2017-09-14 20:08:25 -0700

    Created BattlelogException class

---
+++ app/Exceptions/Adkats/BattlelogException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BFACP\Exceptions\Adkats;
+
+
+use Exception;
+
+/**
+ * Class BattlelogException
+ * @package BFACP\Exceptions\Adkats
+ */
+class BattlelogException extends Exception
+{
+
+}
