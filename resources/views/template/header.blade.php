8c55f409642218886dc9fbf849af06d04a897528
Prophet731 <webmaster@amurray.me>
2017-10-16 20:07:42 -0700

    Fixed invalid links

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -14,10 +14,7 @@
 <meta name="msapplication-TileImage" content="{{ url('images/favicon/mstile-144x144.png') }}">
 <!-- For Windows Phone -->
 <!-- CORE CSS-->
-<link href="{{ url('css/themes/semi-dark-menu/materialize.min.css') }}" type="text/css" rel="stylesheet">
-<link href="{{ url('css/themes/semi-dark-menu/materialize.min.css') }}" type="text/css" rel="stylesheet">
-<!-- Custome CSS-->
-<link href="{{ url('css/custom/custom/css') }}" type="text/css" rel="stylesheet">
+<link href="{{ url('css/themes/semi-dark-menu/materialize.css') }}" type="text/css" rel="stylesheet">
 <!-- INCLUDED PLUGIN CSS ON THIS PAGE -->
 <link href="{{ url('vendors/perfect-scrollbar/perfect-scrollbar.css') }}" type="text/css" rel="stylesheet">
 <link href="{{ url('vendors/flag-icon/css/flag-icon.min.css') }}" type="text/css" rel="stylesheet">
