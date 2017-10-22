d0047e38a800a6b76993e6d56b507fd44095248e
Prophet731 <webmaster@amurray.me>
2017-10-22 02:59:50 -0700

    Created Player Stats Model

---
+++ app/Realm/Player/Stats.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace BFACP\Realm\Player;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Player
+ * @package BFACP\Realm
+ */
+class Stats extends Model
+{
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * @var string
+     */
+    protected $table = 'tbl_playerstats';
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'StatsID';
+
+    /**
+     * @var array
+     */
+    protected $guarded = ['*'];
+
+    /**
+     * @var array
+     */
+    protected $with = [];
+
+    /**
+     * @var array
+     */
+    protected $dates = ['FirstSeenOnServer', 'LastSeenOnServer'];
+
+    /**
+     * @var array
+     */
+    protected $appends = ['first_seen', 'last_seen'];
+
+    /**
+     * @return mixed
+     */
+    public function getFirstSeenAttribute()
+    {
+        return $this->FirstSeenOnServer->toIso8601String();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastSeenAttribute()
+    {
+        return $this->LastSeenOnServer->toIso8601String();
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function server()
+    {
+        return $this->belongsToMany(Server::class, 'tbl_server_player', 'StatsID',
+            'ServerID');
+    }
+}
