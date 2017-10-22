2d3062b728c78b09ef2a87efaaf4f630cffd3413
Prophet731 <webmaster@amurray.me>
2017-10-22 02:58:26 -0700

    Menu Middleware

---
+++ app/Http/Middleware/MenuMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace BFACP\Http\Middleware;
+
+use Closure;
+use Lavary\Menu\Facade as Menu;
+
+/**
+ * Class MenuMiddleware
+ * @package BFACP\Http\Middleware
+ */
+class MenuMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        Menu::make('SideNav', function($m) {
+            $m->add('Home', ['route' => 'home']);
+            $m->add('Players', ['route' => 'player.listing']);
+        });
+
+        return $next($request);
+    }
+}
